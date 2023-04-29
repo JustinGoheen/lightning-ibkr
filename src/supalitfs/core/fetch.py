4fe1bd654e10bbd1046daf25396f8fff355b2e28
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-04-29 16:12:22 -0400

    update app

---
+++ src/supalitfs/core/fetch.py
@@ -0,0 +1,175 @@
+# Copyright Justin R. Goheen.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import datetime
+import os
+import sys
+from calendar import monthrange
+from datetime import datetime as dt
+
+import ib_insync as ib
+import pandas as pd
+from rich import print as rprint
+from rich.progress import Progress
+
+from supalitfs.utilities.front_month import EquityFutureFrontMonth
+
+
+class FetchWork:
+    """a custom LightningWork to fetch data from IBKR"""
+
+    def __init__(
+        self,
+        market: str = "MES",
+        expiry: str = EquityFutureFrontMonth.expiry,
+        current_month: str = "04",
+        duration: str = "1 D",
+        bar_size: str = "5 secs",
+        what_to_show: str = "Trades",
+        use_rth: bool = True,
+        local_host: str = "127.0.0.1",
+        port: str = "7497",
+        datadir: str = "data",
+    ):
+        # init the parent class
+        # super().__init__(parallel=False, cache_calls=True)
+        # create an TWS API object
+        self._ib = ib.IB()
+        # set data settings
+        self.market = market
+        self.expiry = expiry
+        self.current_month = current_month
+        self.duration = duration
+        self.bar_size = bar_size
+        self.what_to_show = what_to_show
+        self.use_rth = use_rth
+        self.datadir = datadir
+        self.end_date_time = None
+        # set API connection settings
+        self.ib_host = local_host
+        self.ib_port = port
+        self.app_id = 1001
+        # create the market contract that will be requested
+        self._marketcontract = ib.Future(self.market, exchange="CME", lastTradeDateOrContractMonth=self.expiry)
+        # create the dataframe that will store bars
+        columns = ["date", "open", "high", "low", "close", "volume", "average", "barCount"]
+        self._marketdata = pd.DataFrame(columns=columns)
+        # connect to TWS
+        self._ib.connect(self.ib_host, self.ib_port, self.app_id)
+
+    def _futuresbars(self) -> pd.DataFrame:
+        """requests historical bars and returns a dataframe"""
+
+        _data = self._ib.reqHistoricalData(
+            self._marketcontract,
+            endDateTime=self.end_date_time,
+            durationStr=self.duration,
+            barSizeSetting=self.bar_size,
+            whatToShow=self.what_to_show,
+            useRTH=self.use_rth,
+        )
+
+        self._ib.sleep(5)  # let bars fill
+
+        _data = ib.util.df(_data)
+
+        return _data
+
+    def _days_to_fetch(self) -> list:
+        # make a list of days to retrieve
+        days = []
+
+        if int(self.current_month) == dt.today().month:
+            endofrange = dt.today().day + 1
+        else:
+            endofrange = monthrange(dt.today().year, int(self.current_month))[1] + 1
+
+        with Progress() as progress:
+            task = progress.add_task("CREATING DAYS TO FETCH", total=len(range(endofrange)))
+            for i in reversed(range(endofrange)):
+                lastday = str(dt.today().year) + self.current_month + str(endofrange - 1)
+                lastday = dt.strptime(lastday, "%Y%m%d").date()
+                offset = datetime.timedelta(days=i)
+                offsetday = lastday - offset
+                tradingday = str(offsetday).replace("-", "")
+                # the time in the IBKR is set to 00:00 of t+1 so we have to use
+                # tuesday through saturday to get monday through friday
+                # if weekday is tuesday through saturday and the month is in the
+                # target month and the day is not the 1st of the month
+                if (
+                    (offsetday.weekday() >= 1)
+                    and (offsetday.weekday() < 6)
+                    and (offsetday.month == int(self.current_month))
+                    and (offsetday.day != 1)
+                ):
+                    days.append(str(dt.strptime(tradingday, "%Y%m%d")).replace("-", ""))
+
+                progress.advance(task)
+
+        # account for end of month days that fall on a weekday and need end of month + 1
+        if lastday.weekday() <= 5:
+            offset = datetime.timedelta(days=1)
+            offsetday = lastday + offset
+            tradingday = str(offsetday).replace("-", "")
+            days.append(str(dt.strptime(tradingday, "%Y%m%d")).replace("-", ""))
+
+        return days
+
+    def _fetch_data(self):
+        # fetch data
+        days = self._days_to_fetch()
+        with Progress() as progress:
+            task = progress.add_task("FETCHING DATA", total=len(days))
+            for day in days:
+                self.end_date_time = day
+                _bars = self._futuresbars()
+                self._marketdata = pd.concat([self._marketdata, _bars], ignore_index=True)
+                progress.advance(task)
+
+    def _prep_data_for_storage(self):
+        # prep data for storage
+        self._marketdata.set_index("date", inplace=True)
+        self._marketdata.sort_index(inplace=True)
+
+    def _store_data_locally(self):
+        # create path names
+        symbolpath = os.path.join(self.datadir, self._marketcontract.symbol)
+        yearpath = os.path.join(symbolpath, self.expiry[:4])
+        monthpath = os.path.join(yearpath, self.current_month)
+        # create directories if they do not exist
+        for dir in [self.datadir, symbolpath, yearpath, monthpath]:
+            if not os.path.exists(dir):
+                os.mkdir(dir)
+        # create filepath name
+        filepath = "".join([monthpath, os.sep, f"historical_{self.bar_size.replace(' ', '_')}_bars.pq"])
+        # save dataframe as parquet file format
+        self._marketdata.to_parquet(filepath)
+
+    def run(self) -> None:
+        """runs the LightningWork from a Lightning Flow"""
+        rprint(f"[{dt.now().time()}] RUNNING")
+        # run ops
+        rprint(f"[{dt.now().time()}] FETCHING DATA")
+        self._fetch_data()
+        rprint(f"[{dt.now().time()}] PREPARING FOR STORAGE")
+        self._prep_data_for_storage()
+        rprint(f"[{dt.now().time()}] SAVING")
+        self._store_data_locally()
+        rprint(f"[{dt.now().time()}] STOPPING")
+        self.stop()
+
+    def stop(self):
+        self._ib.disconnect()
+        sys.exit()
---
+++ src/supalitfs/core/store.py
@@ -0,0 +1,216 @@
+# Copyright Justin R. Goheen.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import abc
+import os
+from pathlib import Path
+from typing import Any, List
+
+import supabase
+from lightning import CloudCompute, LightningWork
+
+
+class SupabaseStorage(LightningWork, abc.ABC):
+    """A custom LightningWork to Drive Supabase Bucket Operations
+
+    # Notes
+        - LightningWork [docs](https://lightning.ai/docs/app/stable/core_api/lightning_work/lightning_work.html).
+        - CloudCompute [docs](https://lightning.ai/docs/app/stable/core_api/lightning_work/compute.html#cloudcompute).
+        - Supabase-Py [docs](https://supabase.com/docs/reference/python/initializing).
+    """
+
+    def __init__(
+        self,
+        parallel=False,
+        cache_calls=True,
+        raise_exception=True,
+        host="127.0.0.1",
+        port=None,
+        local_build_config=None,
+        cloud_build_config=None,
+        start_with_flow=True,
+        cloud_compute_name="default",
+        cloud_compute_disk_size=0,
+        cloud_compute_idle_timeout=60,
+        cloud_compute_shm_size=0,
+        cloud_compute_interruptible=False,
+        cloud_compute_internal_id=None,
+    ):
+
+        super().__init__(
+            parallel=parallel,
+            cache_calls=cache_calls,
+            raise_exception=raise_exception,
+            host=host,
+            port=port,
+            local_build_config=local_build_config,
+            cloud_build_config=cloud_build_config,
+            start_with_flow=start_with_flow,
+            cloud_compute=CloudCompute(
+                name=cloud_compute_name,
+                disk_size=cloud_compute_disk_size,
+                idle_timeout=cloud_compute_idle_timeout,
+                shm_size=cloud_compute_shm_size,
+                interruptible=cloud_compute_interruptible,
+                _internal_id=cloud_compute_internal_id,
+            ),
+        )
+
+    @property
+    def available_buckets(self):
+        """shows buckets available to user
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-listbuckets
+        """
+        return supabase.storage().list_buckets()
+
+    def create_bucket(self, bucket_name: str):
+        """creates a bucket
+
+        # Arguments
+            bucket_name: the name to use when creating the bucket
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-createbucket
+        """
+        supabase.storage().create_bucket(bucket_name)
+
+    def retrieve_bucket(self, bucket_name: str):
+        """retrieves a bucket
+
+        # Arguments
+            bucket_name: the name of the bucket to retrieve
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-getbucket
+        """
+        return supabase.storage().get_bucket(bucket_name)
+
+    def create_signed_url(self, bucket_name: str, destination: Path | str, expiry_duration: int):
+        """creates a signed url
+
+        # Arguments
+            bucket_name:
+            destination:
+            expiray_duration: the length of time in seconds to keep the url alive
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-from-createsignedurl
+        """
+        supabase.storage().from_(bucket_name).create_signed_url(destination, expiry_duration)
+
+    def retrieve_public_url(self, bucket_name: str, source: Path | str):
+        """Returns the URL for an asset in a public bucket
+
+        # Arguments
+            bucket_name: the name of the bucket
+            source:
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-from-getpublicurl
+        """
+        supabase.storage().from_(bucket_name).get_public_url(source)
+
+    def list_bucket_files(self, bucket_name: str) -> List[str]:
+        """lists files in a user defined bucket
+
+        # Arguments
+            bucket_name: the name of the bucket to show the contents of
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-from-list
+        """
+        return supabase.storage().from_(bucket_name).list()
+
+    def delete_files_from_bucket(self, bucket_name: str, files: List[Path | str]) -> None:
+        """deletes user defined files from specified bucket
+
+        # Arguments
+            bucket_name: the name of the bucket where the files are located
+            files: a Python list of files, including file format ending
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-deletebucket
+        """
+        supabase.storage().from_(bucket_name).move(*files)
+
+    def move_files(self, bucket_name: str, files: List[Path | str]) -> None:
+        """moves a list of user defined files
+
+        # Arguments
+            bucket_name: the name of the bucket where the files are located
+            files: a Python list of files, including file format ending
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-from-move
+        """
+        supabase.storage().from_(bucket_name).move(*files)
+
+    def delete_bucket(self, bucket_name: str) -> None:
+        """deletes a user defined bucket
+
+        # Arguments
+            bucket_name: the name of the bucket to delete
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-deletebucket
+        """
+        supabase.storage().delete_bucket(bucket_name)
+
+    def empty_bucket(self, bucket_name: str) -> None:
+        """empties a user defined bucket
+
+        # Arguments
+            bucket_name: the name of the bucket to empty
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-emptybucket
+        """
+        supabase.storage().empty_bucket(bucket_name)
+
+    def upload_to_bucket(self, source: Path | str, destination: Path | str, bucket_name: str) -> None:
+        """uploads a file to bucket
+
+        # Arguments
+            source:
+            destination:
+            bucket_name:
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-from-upload
+        """
+        with open(source, "rb+"):
+            supabase.storage().from_(bucket_name).upload(destination, os.path.abspath(source))
+
+    def download_from_bucket(self, source: Path | str, bucket_name: str) -> Any:
+        """downloads a file from bucket to a destination
+
+        # Arguments
+            source: the object to download
+            bucket_name: the name of the bucket where source is located
+
+        # Notes
+            see: https://supabase.com/docs/reference/python/storage-from-download
+        """
+
+        with open(bucket_name, "wb+") as f:
+            res = supabase.storage().from_(bucket_name).download(source)
+            f.write(res)
+
+    def mount(self) -> None:
+        """mount Supabase bucket to Lightning"""
+
+    def run(self) -> None:
+        """defines run for LightningFlow"""
